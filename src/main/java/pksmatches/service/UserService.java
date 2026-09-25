55616c1924cbd9d6836d3e73642a168a18c9fa3e
Ivan Sidelnikov <ivan.sidelnikov21@yandex.ru>
2026-09-25 11:58:16 +0300

    UserService.java Update

--- src/main/java/pksmatches/service/UserService.java
+++ src/main/java/pksmatches/service/UserService.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 public class UserService {
+
     private final UserRepository userRepository;
 
     public UserService(UserRepository userRepository) {
